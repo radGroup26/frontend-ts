429a2995f243b2d996c87520ef065e05bace45b8
desynkd <ravindupeeris180@gmail.com>
2024-09-03 12:53:49 +0530

    Add Order Add functionality to DiningTable

--- src/components/DiningTable.tsx
+++ src/components/DiningTable.tsx
@@ -14,11 +14,20 @@ interface DiningTableProps {
 }
 
 export default function DiningTable({ tableId, tableNo, tableSeats }: DiningTableProps) {
+    const { selectedRestaurant } = useAuth();
     const [orders, setOrders] = useState([]);
     const [orderTypes, setOrderTypes] = useState([]);
-    const [newOrder, setNewOrder] = useState({ order: "", status: "" });
-    const { selectedRestaurant } = useAuth();
+    const [newOrder, setNewOrder] = useState({ restaurantId: selectedRestaurant._id,
+        tableId:tableId, name: "", quantity: 0, status: "Pending" });
+    const [deleteOrder, setDeleteOrder] = useState("");
     const token = localStorage.getItem('accessToken');
+    const quantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // const allowedRoles = ["admin", "chef", "waiter"];
+    // console.log(orderTypes[0].items)
+    // console.log(orders)
+    // console.log(tableId)
+
 
     useEffect(() => {
         // // Fetch orders from the backend
@@ -44,33 +53,43 @@ export default function DiningTable({ tableId, tableNo, tableSeats }: DiningTabl
             .catch(error => {
                 console.error("Error fetching order types:", error);
             });
-        //
-        // // Sample orders array
-        // const sampleOrders = [
-        //     { id: "1", order: "Teriyaki Pizza Large", status: "Completed" },
-        //     { id: "2", order: "Spicy Chicken Pizza Regular", status: "Pending" },
-        //     { id: "3", order: "Classic Pizza Extra Cheese Small", status: "In Progress" },
-        // ];
-        // setOrders(sampleOrders);
-    }, []);
+    }, [newOrder]);
 
+    // handle Waiter's Add order Submit
     const handleAddOrder = (e) => {
         e.preventDefault();
-        axios.post("http://localhost:3000/orders", newOrder)
+        axios.post("http://localhost:3000/orders/create", newOrder, {
+            headers: {
+                'Authorization': `Bearer ${token}`}
+        })
             .then(response => {
                 setOrders([...orders, response.data]);
-                setNewOrder({ order: "", status: "Pending" });
+                setNewOrder({ restaurantId: selectedRestaurant._id,
+                    tableId:tableId, name: "", quantity:0, status: "Pending" });
             })
             .catch(error => {
                 console.error("Error adding order:", error);
             });
     };
 
-    // const allowedRoles = ["admin", "chef", "waiter"];
-    // console.log(orderTypes[0].items)
-    // console.log(orders)
-    // console.log(tableId)
+    // handle Chef's Delete order Submit
+    const handleDeleteOrder = (e) => {
+        e.preventDefault();
+        // axios.post("http://localhost:3000/orders/create", newOrder, {
+        //     headers: {
+        //         'Authorization': `Bearer ${token}`}
+        // })
+        //     .then(response => {
+        //         setOrders([...orders, response.data]);
+        //         setNewOrder({ restaurantId: selectedRestaurant._id,
+        //             tableId:tableId, name: "", quantity:0, status: "Pending" });
+        //     })
+        //     .catch(error => {
+        //         console.error("Error adding order:", error);
+        //     });
+    };
 
+    // Display orders
     let orderContent;
     if (orders.length > 0) {
         orderContent = (
@@ -93,6 +112,74 @@ export default function DiningTable({ tableId, tableNo, tableSeats }: DiningTabl
         )
     }
 
+    // Waiters Footer Content
+    let addOrderContent =(
+        <Popover>
+            <PopoverTrigger className="absolute right-10 mb-5">Add New Order</PopoverTrigger>
+            <PopoverContent>
+                <form onSubmit={handleAddOrder} className={"flex flex-col"}>
+                    <div className={"m-1"}>
+                        <Select onValueChange={(value) => setNewOrder({...newOrder, name: value})}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select Order Type"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                {orderTypes.map((type) => (
+                                    <SelectItem key={type._id} value={type.name + ' ' + type.option}>
+                                        {type.name + ' ' + type.option}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className={"m-1"}>
+                        <Select  defaultValue={quantity[0].toString()}
+                                 onValueChange={(value) => setNewOrder({...newOrder, quantity: Number(value)})}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select Quantity"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                {quantity.slice().reverse().map((quantity) => (
+                                    <SelectItem key={quantity} value={quantity.toString()}>
+                                        {quantity}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <Button type="submit" className={'my-3 mx-1'}>Add Order</Button>
+                </form>
+            </PopoverContent>
+        </Popover>
+    )
+
+    // Chef Footer Content
+    // let deleteOrderContent =(
+    //     <Popover>
+    //         <PopoverTrigger className="absolute right-10 mb-5">Delete Order</PopoverTrigger>
+    //         <PopoverContent>
+    //             <form onSubmit={handleDeleteOrder} className={"flex flex-col"}>
+    //                 <div className={"m-1"}>
+    //                     <Select onValueChange={(value) => setNewOrder({...newOrder, name: value})}>
+    //                         <SelectTrigger>
+    //                             <SelectValue placeholder="Select Order"/>
+    //                         </SelectTrigger>
+    //                         <SelectContent>
+    //                             {orders.map((order) => (
+    //                                 <SelectItem key={order._id} value={order._id}>
+    //                                     {order.name + " " + order.quantity}
+    //                                 </SelectItem>
+    //                             ))}
+    //                         </SelectContent>
+    //                     </Select>
+    //                 </div>
+    //                 <Button type="submit" className={'my-3 mx-1'}>Delete Order</Button>
+    //             </form>
+    //         </PopoverContent>
+    //     </Popover>
+    // )
+
+
     return (
         <Card className={'flex flex-col min-w-96 min-h-96 m-3'}>
             <CardHeader>
@@ -103,26 +190,7 @@ export default function DiningTable({ tableId, tableNo, tableSeats }: DiningTabl
                 {orderContent}
             </CardContent>
             <CardFooter className="relative">
-                <Popover>
-                    <PopoverTrigger className="absolute right-10 mb-5">Add New Order</PopoverTrigger>
-                    <PopoverContent>
-                        <form onSubmit={handleAddOrder} className={"flex flex-col"}>
-                            <Select onValueChange={(value) => setNewOrder({...newOrder, order: value})}>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select Order Type"/>
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {orderTypes.map((type) => (
-                                        <SelectItem key={type._id} value={type.name + ' ' + type.option}>
-                                            {type.name + ' ' + type.option}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <Button type="submit" className={'mt-4'}>Add Order</Button>
-                        </form>
-                    </PopoverContent>
-                </Popover>
+                {addOrderContent}
             </CardFooter>
         </Card>
     )
