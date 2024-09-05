b8796049a3ad034a88b01806dcdd5432377a48e1
Tharushi-Kavindi <130686389+Tharushi-Kavindi@users.noreply.github.com>
2024-09-05 13:10:05 +0530

    modify the code

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -206,7 +206,7 @@ function Notification() {
                   <textarea
                     placeholder="Enter the Message..."
                     id="message"
-                    className="flex h-32 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                    className="flex h-32 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm disabled:opacity-50"
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
                     required
