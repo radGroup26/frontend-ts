81d6c5ddac2689b9f3af6df2cc189b89f22666e4
desynkd <ravindupeeris180@gmail.com>
2024-09-06 23:22:38 +0530

    Add functionality for Table Tab

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -2,27 +2,37 @@ import {useEffect, useState} from "react";
 import DiningTable from "@/components/DiningTable";
 import {useAuth} from "@/context/AuthContext.tsx";
 import axios from "axios";
+import api from "@/lib/api/api.ts";
+import {Table} from "@/types/Table.tsx";
 
 export default function Dashboard() {
     const { selectedRestaurant } = useAuth();
-    const [tables, setTables] = useState([]);
+    const [tables, setTables] = useState<Table[]>([]);
 
     useEffect(() => {
         const getTables = async () => {
             // console.log(selectedRestaurant._id)
-
-            try {
-                const token = localStorage.getItem('accessToken');
-                const response = await axios.get(`http://localhost:3000/restaurants/${selectedRestaurant._id}/tables`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
+            api.get(`/tables/${selectedRestaurant?._id}`)
+                .then(response => {
+                    setTables(response.data);
+                })
+                .catch(error => {
+                    console.error("Error fetching order:", error);
                 });
-                console.log(response.data);
-                setTables(response.data);
-            } catch (error) {
-                console.error("Error fetching tables:", error);
-            }
+
+            // try {
+            //     const token = localStorage.getItem('accessToken');
+            //     const response = await api.get(`http://localhost:3000/restaurants/${selectedRestaurant._id}/tables`, {
+            //         headers: {
+            //             'Authorization': `Bearer ${token}`
+            //         }
+            //     });
+            //     console.log(response.data);
+            //     setTables(response.data);
+            // } catch (error) {
+            //     console.error("Error fetching tables:", error);
+            // }
+
         };
 
         if (selectedRestaurant) {
@@ -31,7 +41,7 @@ export default function Dashboard() {
     }, [selectedRestaurant]);
 
     return (
-        <div className={"flex"}>
+        <div className={"flex flex-wrap"}>
             {tables.map((table) => (
                 <DiningTable key={table._id} tableId={table._id} tableNo={table.no} tableSeats={table.seats} />
             ))}
