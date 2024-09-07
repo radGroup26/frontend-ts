d0c1e900651f32ba59995818b04ae9f3d39126d4
Rehan Peiris <rehanperis58@gmail.com>
2024-09-07 23:49:09 +0530

    profile update

--- src/routes/profilePage.tsx
+++ src/routes/profilePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ProfileCard from '../components/ProfileCard'; 
 import { useAuth } from '@/context/AuthContext';
-import axios from 'axios';
+import api from "@/lib/api/api.ts";
 interface User {
   First_Name: string;
   Last_Name: string;
@@ -23,25 +23,36 @@ const ProfilePage: React.FC = () => {
     setUser({name: 'John Doe', email: 'johndoe@gmail.com', role: 'admin'});
   }, []);  */
 
-  useEffect(() => {
+  /* useEffect(() => {
     const userID = authUser?.userId;
 
     const getDetails = async () => {
       try {
         const token = localStorage.getItem('accessToken');
-        const response = await axios.get(`http://localhost:3000/profiles/${userID}`, {
+        const response = await api.get(`/profiles/${userID}`, {
           headers: {
             'Authorization': `Bearer ${token}`
           }
         });
-        console.log(response.data);
         setUser({ First_Name: response.data.first_name, Last_Name: response.data.last_name, email: response.data.email, role: response.data.role });
       } catch (error) {
         console.error('Error fetching user details:', error);
       }
     };
     getDetails();
-  }, []); 
+  }, []);  */
+
+  useEffect(() => {
+    const userID = authUser?.userId;
+    api.get(`/profiles/${userID}`)
+        .then(response => {
+          console.log(response.data);
+            setUser({ First_Name: response.data.first_name, Last_Name: response.data.last_name, email: response.data.email, role: response.data.role });
+        })
+        .catch(error => {
+            console.error("Error fetching order types:", error);
+        });
+}, []);
 
   return (
     <div className="container mx-auto py-8">
